a979e547a76f9921ff374a2f9ab45d64e2b4b024
Naoki Saito <saito.naoki@nmiri.city.nagoya.jp>
2017-11-05 22:04:24 +0900

    modified docker-compose.yml and start_shell.sh in order to pull image from dockerhub

--- start_shell.sh
+++ start_shell.sh
@@ -2,10 +2,16 @@
 
 set -eu
 
-SERVICE=python-dev-nvidia
+if [ $# -ne 1 ]; then
+  echo "start_shell.sh <service_name>"
+  echo "available services: python-dev-nvidia"
+  exit 1
+fi
+
+SERVICE=$1
 
 if [ "x$(docker-compose ps -q ${SERVICE})" = "x" ]; then
-  docker-compose build ${SERVICE}
+  docker-compose pull ${SERVICE}
 fi
 docker-compose up -d ${SERVICE}
 docker exec -i -t `docker-compose ps -q ${SERVICE}` bash --login
